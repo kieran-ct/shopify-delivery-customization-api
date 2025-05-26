7f0a4a6dfb726ffa4f0ffaf5131a5d4c557cb01f
kieran-ct <kieranct@gmail.com>
2025-05-26 21:13:30 +0200

    Explicitly map routes in remix.config.cjs

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,12 +1,27 @@
+// remix.config.cjs
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  // Output as a .cjs file:
-  serverBuildPath: "build/index.cjs",
-  serverModuleFormat: "cjs",
-  serverPlatform: "node",
+  routes: async (defineRoutes) =>
+    defineRoutes((route) => {
+      // Home page at /
+      route("/", "routes/index.tsx");
+
+      // Shopify auth
+      route("auth/login", "routes/auth.login/route.tsx");
+      route("auth/:any", "routes/auth.$.tsx");
+
+      // Webhooks
+      route(
+        "webhooks/app/uninstalled",
+        "routes/webhooks.app.uninstalled.tsx"
+      );
+
+      // Embedded app frame
+      route("app", "routes/app.tsx", (app) => {
+        app.route("additional", "routes/app.additional.tsx");
+      });
+    }),
 };
